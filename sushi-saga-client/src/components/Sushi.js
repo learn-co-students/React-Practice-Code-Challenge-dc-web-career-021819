653f945bb1d97e6f3fe37e189644138b131f74bf
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-29 08:40:09 -0400

    add solution

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  const item = props.item
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
+      <div className="plate"
+           onClick={(event) => props.addIdToEatenSushi(event, props)}
+           data-sushi-id={item.id}>
+        {
+          /* Tell me if this sushi has been eaten! */
+          !props.checkEatenSushi ?
             null
           :
-            <img src={/* Give me an image source! */} width="100%" />
+            <img src={item.img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {item.name} - ${item.price}
       </h4>
     </div>
   )
