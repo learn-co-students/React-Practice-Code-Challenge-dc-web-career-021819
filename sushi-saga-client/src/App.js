9cbe2489566a18c082a542734ea53da7629786ba
seanlucius <seanlucius22@icloud.com>
2019-04-29 11:12:55 -0400

    meh

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  state = {
+    allSushis: [],
+    currentSushi: [],
+    sushiIndex: 0,
+    balance: 100,
+    eaten: []
+  };
+
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(sushis => {
+        this.setState({
+          allSushis: sushis,
+          currentSushi: sushis.slice(0, 4)
+        });
+      });
+  }
+
+  getMoreSushis = () => {
+    const currentIndex = this.state.sushiIndex;
+    this.setState({
+      currentSushi: this.state.allSushis.slice(currentIndex, currentIndex + 4),
+      sushiIndex: this.state.sushiIndex + 4
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer
+          currentSushi={this.state.currentSushi}
+          getMoreSushis={this.getMoreSushis}
+        />
         <Table />
       </div>
     );
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Sushi = (props) => {
+const Sushi = ({ piece }) => {
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+      <div className="plate" onClick={/* Give me a callback! */ null}>
+        {/* Tell me if this sushi has been eaten! */
+        false ? null : <img src={piece.img_url} width="100%" />}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {piece.name} - ${piece.price}
       </h4>
     </div>
-  )
-}
+  );
+};
 
-export default Sushi
+export default Sushi;
